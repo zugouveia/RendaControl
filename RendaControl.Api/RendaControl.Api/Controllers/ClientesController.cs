1b2aa4c2bd56d50e3e692b143be863798420d4ef
zugouveia <fegouveiab@gmail.com>
2026-04-19 23:19:08 -0300

    Integracao com backend

---
+++ RendaControl.Api/RendaControl.Api/Controllers/ClientesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RendaControl.Api.Models;
+
+namespace RendaControl.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Cliente> _clientes = new List<Cliente>();
+
+        // GET
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return _clientes;
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Post([FromBody] Cliente novoCliente)
+        {
+            if (novoCliente == null) return BadRequest();
+
+            novoCliente.Id = _clientes.Count + 1;
+            _clientes.Add(novoCliente);
+
+            return CreatedAtAction(nameof(Get), new { id = novoCliente.Id }, novoCliente);
+        }
+    }
+}
